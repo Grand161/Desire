e05ddce8b1f994fad62ce062117a0a987b45f4b8
Grand161 <melixov2017@yandex.ru>
2021-10-26 08:15:54 +0300

    added section blog

--- app/js/main.js
+++ app/js/main.js
@@ -22,6 +22,15 @@ $(function () {
     slidesToShow: 10,
   });
 
+  $(".article-slider__box").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow:
+      '<button type = "button" class = "article-slider__arrow article-slider__arrowleft"> <img src="images/left-slider.svg" alt="arrow left"></ button>',
+    nextArrow:
+      '<button type = "button" class = "article-slider__arrow article-slider__arrowright"> <img src="images/right-slider.svg" alt="arrow right"></ button>',
+  });
+
   var mixer = mixitup(".gallery__inner", {
     load: {
       filter: ".living",
